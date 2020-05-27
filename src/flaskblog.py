f52bb876fa05f05f94d214bf4551264618d4c1ce
Guilherme Loureiro <loureiro85@gmail.com>
2020-05-26 21:07:08 -0300

    Part 1 - All

--- src/flaskblog.py
+++ src/flaskblog.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Home Page</h1>'
 
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+    
+
+@app.route('/home')
+def home():
+    return '<h1>Home Page</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
